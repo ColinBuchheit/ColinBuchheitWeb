b18a58f0bbbb503fbcfc34b62efefde19f9fab0a
Colin Buchheit <130728626+ColinBuchheit@users.noreply.github.com>
2024-08-25 12:44:39 -0500

    Integrated bot security with reCAPTCHA

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers/rootReducer'; // Import the combined root reducer
+import rootReducer from './reducers/rootReducer'; // Adjust the path as necessary
 
 // Configure and create the Redux store
 const store = configureStore({
   reducer: rootReducer,
-  
 });
+
+// Define the RootState type
+export type RootState = ReturnType<typeof store.getState>;
 
 // Export the store so it can be used in the application
 export default store;
